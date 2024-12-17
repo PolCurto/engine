fd657705697f38625c3a1bfc61a59aa9189fdfc6
PolCurto <polcurto@gmail.com>
2024-12-17 16:01:48 +0100

    Update shaders

--- Game/default_fragment.glsl
+++ Game/default_fragment.glsl
@@ -1,13 +1,44 @@
 #version 460
 
-out vec4 color;
+out vec4 out_color;
 
 in vec2 uv0;
+in vec3 position_normal;
+in vec3 fragment_position;
+
+layout(location = 3) uniform vec3 light_dir;
+layout(location = 4) uniform vec3 ambient;
+layout(location = 5) uniform float Ks;
+layout(location = 6) uniform float Kd;
+layout(location = 7) uniform float shininess;
+layout(location = 8) uniform vec3 light_color;
+layout(location = 9) uniform vec3 view_position; 
 
 layout(binding = 0) uniform sampler2D mytexture;
 
 void main()
 {
-    color = texture2D(mytexture, uv0);
-    //color = vec4(0.0, 1.0, 0.6, 1.0);
+    vec3 n_light_dir = normalize(light_dir);
+    vec3 n_position_normal = normalize(position_normal);
+    float NdotL = dot(n_position_normal, -n_light_dir);
+
+    vec4 diffuse_color = texture2D(mytexture, uv0);
+
+    if (NdotL > 0.0f)
+	{
+		vec3 diffuse = diffuse_color.xyz * light_color * NdotL * Kd;
+		vec3 R = reflect(-n_light_dir, n_position_normal);
+		vec3 V = normalize(view_position - fragment_position);
+		
+		float RdotV = max(dot(R, V), 0.0f);
+		vec3 specular = light_color * pow(RdotV, shininess) * Ks;
+		
+		vec3 color = ambient * diffuse_color.xyz + diffuse + specular;
+	}
+	else
+	{
+		vec3 color = diffuse_color * vec4(ambient, 1.0f);
+	}
+
+    out_color = vec4(color, 1.0f);
 }
--- Game/default_vertex.glsl
+++ Game/default_vertex.glsl
@@ -1,16 +1,21 @@
 #version 460
 
-layout(location = 0) in vec3 my_vertex_position;
+layout(location = 0) in vec3 vertex_position;
 layout(location = 1) in vec2 vertex_uv0;
+layout(location = 2) in vec3 vertex_normals;
 
 layout(location = 0) uniform mat4 projection;
 layout(location = 1) uniform mat4 view;
 layout(location = 2) uniform mat4 model;
 
 out vec2 uv0;
+out vec3 position_normal;
+out vec3 fragment_position;
 
 void main()
 {
-	gl_Position = projection * view * model * vec4(my_vertex_position, 1.0);
+	gl_Position = projection * view * model * vec4(vertex_position, 1.0f);
 	uv0 = vertex_uv0;
+	fragment_position = (model * vec4(vertex_position), 1.0f).xyz;
+	position_normal = transpose(inverse(mat3(model))) * vertex_normals;
 }
